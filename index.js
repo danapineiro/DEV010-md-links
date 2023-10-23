4a61c9e834240ddfaf101fc220b331a809c80f44
danapineiro <danapineiro@icloud.com>
2023-10-23 18:36:16 -0300

    Promesa y Funciones de ruta adsoluta y convertir a adsoluta

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
-module.exports = () => {
-  // ...
+// Se obtiene la ruta del archivo
+// import
+const path = require("./lib/app.js");
+
+// Aqui la funcion mdLinks
+const mdLinks = (route) => new Promise((resolve, reject) => {
+    if (path.isAbsolute(route)) {
+        resolve(route);
+    } else {
+        const adsolutePath = path.resolve(route);
+        if(!adsolutePath){
+            resolve(adsolutePath);
+        } else {
+            reject("La ruta no es valida.");
+        }
+    }
+});
+console.log(mdLinks);
+
+
+
+
+/*const app = require ("./lib/app");
+const path = require ("path");
+const fs = require ("fs");
+
+const mdLinks = (route) => {
+    return new Promise ((resolve, reject) => {
+        if (path.isAbsolute(route)){
+            resolve(route);
+        } else {
+            const adsolutePath = path.resolve(route);
+            fs.access(adsolutePath, (error) => {
+                if (error) {
+                    reject("La ruta no es valida.");
+                } else {
+                    resolve(adsolutePath);
+                }
+            });
+        }
+    });
 };
+module.exports = {
+    mdLinks,
+};*/
