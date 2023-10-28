c14ebe806be1c833476add8b56fec762328b81f6
danapineiro <danapineiro@icloud.com>
2023-10-27 23:21:06 -0300

    Funcionesde leer extraer links y parte de la promesa mejorada

--- examples/README.md
+++ examples/README.md
@@ -1,2 +1,3 @@
 https://developer.mozilla.org/es/docs/Glossary/Callback_function
 https://jestjs.io/docs/es-ES/getting-started
+[Node.js](https://nodejs.org/es/)
--- index.js
+++ index.js
@@ -1,32 +1,58 @@
 // Se obtiene la ruta del archivo
 // import
-const path = require("./lib/app.js");
-const fs = require("fs");
+const fnApp = require("./lib/app.js");
+
 
 // Aqui la funcion mdLinks
 // Definición de la función mdLinks que verifica y resuelve rutas
 const mdLinks = (route) => new Promise((resolve, reject) => {
-    // 1: Verifica si la ruta proporcionada es absoluta
-    if (path.isAbsolute(route)) {
-        // 2: Si es absoluta, resuelve la promesa con la misma ruta
-        resolve(route);
-    } else {
-        // 3: Si la ruta no es absoluta, intenta resolverla
-        const absolutePath = path.resolve(route);
-
-        // 4: Verificar si la ruta resuelta existe en el sistema
-        if (fs.existsSync(absolutePath)) {
-        // Si existe, resuelve la promesa con la ruta absoluta resultante
-            resolve(absolutePath);
-        } else {
-        // Si no se puede resolver la ruta, rechaza la promesa con un mensaje de error
-            reject("La ruta no es valida o no existe en el sistema");
+    const asbRoute = fnApp.transformRoute(route);
+    if(fnApp.routeExists(asbRoute)){
+        if(fnApp.isMarkdownFile(asbRoute)){
+            // Leer el contenido
+            const markdownContent = fnApp.readMarkdownFile(asbRoute);
+            if (markdownContent !== null) {
+                console.log(15, fnApp.isUrlValid(markdownContent));
+                resolve(markdownContent);
+            }else{
+                reject("Archivo vacio, prueba con otro");
+            }
+           
+        }else{
+            reject("Archivo invalido, prueba con un archivo md");
         }
+        
+    }else{
+        reject("Tu ruta no existe prueba con otra");
     }
+    
+    // 1: Verifica si la ruta proporcionada es absoluta
+    // if (path.isAbsoluteRoute(route)) {
+    //     // 2: Si es absoluta, resuelve la promesa con la misma ruta
+    //     resolve(route); 
+    // } else {
+    //     // 3: Si la ruta no es absoluta, intenta resolverla
+    //     const absolutePath = path.resolve(route);
+
+    //     // 4: Verificar si la ruta resuelta existe en el sistema
+    //     if (fs.existsSync(absolutePath)) {
+    //     // Si existe, resuelve la promesa con la ruta absoluta resultante
+    //         resolve(absolutePath);
+    //     } else {
+    //     // Si no se puede resolver la ruta, rechaza la promesa con un mensaje de error
+    //         reject("La ruta no es valida o no existe en el sistema");
+    //     }
+    // }
 });
 // Imprime la definición de la función mdLinks en la consola
-console.log("Función mdLinks aloja mi promesa");
-console.log(mdLinks);
+// console.log("Función mdLinks aloja mi promesa");
+// console.log(mdLinks);
+const route = "./examples/README.md";
+mdLinks(route).then((res)=>{
+    console.log(res, "******************");
+}).catch((err)=>{
+    console.log(err);
+});
 
 
 
